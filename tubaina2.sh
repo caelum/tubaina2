cf6cdb95fdbf65e9acd7abaac5f3d2b29bd8e9ff
Sérgio Lopes <sergio.lopes@caelum.com.br>
2015-05-25 19:11:19 -0300

    first script

---
+++ tubaina2.sh
@@ -0,0 +1,78 @@
+#!/bin/bash
+# Execute na pasta do projeto
+
+
+SRCDIR="$(pwd)"
+BUILDDIR="$SRCDIR"/.build
+
+rm -rf "$BUILDDIR"
+mkdir -p "$BUILDDIR"
+
+# copy directory
+echo "Copying project to $BUILDDIR"
+cp -R "$SRCDIR"/* "$BUILDDIR"/
+
+# first chapter as README
+first_chapter="$(ls *.md | sort -n | head -1)"
+echo "Renaming $first_chapter to README"
+if [ -f "$first_chapter" ]; then
+	mv "$BUILDDIR"/"$first_chapter" "$BUILDDIR"/README.md
+fi
+
+# generates SUMMARY
+echo "Generating SUMMARY"
+echo "# Sumário" > "$BUILDDIR"/SUMMARY.md
+
+for file in *.md; do
+	title=$(head -1 "$file" | sed -e 's/^#[ \t]*//g')
+
+	if [ "$file" == "$first_chapter" ]; then
+		file="README.md"
+	fi
+
+	echo "  * $title ($file)"
+	echo "* [$title]($file)" >> "$BUILDDIR"/SUMMARY.md
+
+	# Remove first line (chapter title)
+	tail -n +2 "$BUILDDIR"/"$file" > "$BUILDDIR"/.tmp
+	mv "$BUILDDIR"/.tmp "$BUILDDIR"/"$file"
+done
+
+# Get book info
+if [ -f "$SRCDIR"/../book.properties ]; then
+	. "$SRCDIR"/../book.properties
+fi
+. "$BUILDDIR"/book.properties
+
+# book.json
+cat <<END > "$BUILDDIR"/book.json
+	{
+		"title": "$TITLE",
+		
+		"description": "$DESCRIPTION",
+		
+		"author": "$AUTHOR",
+
+		"publisher": "$PUBLISHER",
+		
+		"pdf": {
+			"margin": {
+				"right": 62,
+				"left": 62,
+				"top": 62,
+				"bottom": 62
+			},
+			"headerTemplate": "<p id='ebook-header' style='border-bottom: 1px solid black; margin-top: 36pt;'><span class='odd_page'><span>Casa do Código</span><span style='float:right'>_SECTION_</span></span><span class='even_page'><span>_SECTION_</span><span style='float:right'>Casa do Código</span></span><script>if(!(/^[0-9]/.test('_SECTION_'))) { document.getElementById('ebook-header').style.display='none'; }</script></p>",
+			"footerTemplate": "<p id='ebook-footer'></p><script>var footer = document.getElementById('ebook-footer'); footer.innerHTML = _PAGENUM_ - 2; if(_PAGENUM_ % 2 != 0){ footer.style.textAlign = 'right'; }</script>"
+		},
+
+		"plugins": ["cdc", "$TEMA"],
+		
+		"links": {
+			"gitbook": false
+		}
+	}
+END
+
+# Build PDF
+docker run -v "$BUILDDIR":/data cdc/gitbook gitbook pdf
