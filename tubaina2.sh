b16fecd086b42e228e3f8549d0fc535a1ec9e3d0
Sérgio Lopes <sergio.lopes@caelum.com.br>
2015-05-26 14:58:43 -0300

    optional cover and parameters

--- tubaina2.sh
+++ tubaina2.sh
@@ -32,6 +32,7 @@ echo "Generating SUMMARY"
 echo "# Sumário" > "$BUILDDIR"/SUMMARY.md
 
 for file in *.md; do
+	# Extract first line (expects h1 syntax)
 	title=$(head -1 "$file" | sed -e 's/^#[ \t]*//g')
 
 	if [ "$file" == "$first_chapter" ]; then
@@ -50,37 +51,48 @@ done
 if [ -f "$SRCDIR"/../book.properties ]; then
 	. "$SRCDIR"/../book.properties
 fi
-. "$BUILDDIR"/book.properties
+if [ -f "$BUILDDIR"/book.properties ]; then
+	. "$BUILDDIR"/book.properties
+fi
+
+# Default book info
+[ $TITLE ] || TITLE="Untitled {define one in book.properties}"
+[ $DESCRIPTION ] || DESCRIPTION="No description {define one in book.properties}"
+[ $AUTHOR ] || AUTHOR="Anonymous {define an author in book.properties}"
+[ $PUBLISHER ] || PUBLISHER="Anonymous {define a publisher in book.properties}"
+[ $THEME ] || THEME="cdc-tema"
 
 # book.json
 cat <<END > "$BUILDDIR"/book.json
-	{
-		"title": "$TITLE",
-		
-		"description": "$DESCRIPTION",
-		
-		"author": "$AUTHOR",
-
-		"publisher": "$PUBLISHER",
-		
-		"pdf": {
-			"margin": {
-				"right": 62,
-				"left": 62,
-				"top": 62,
-				"bottom": 62
-			},
-			"headerTemplate": "<p id='ebook-header' style='border-bottom: 1px solid black; margin-top: 36pt;'><span class='odd_page'><span>Casa do Código</span><span style='float:right'>_SECTION_</span></span><span class='even_page'><span>_SECTION_</span><span style='float:right'>Casa do Código</span></span><script>if(!(/^[0-9]/.test('_SECTION_'))) { document.getElementById('ebook-header').style.display='none'; }</script></p>",
-			"footerTemplate": "<p id='ebook-footer'></p><script>var footer = document.getElementById('ebook-footer'); footer.innerHTML = _PAGENUM_ - 2; if(_PAGENUM_ % 2 != 0){ footer.style.textAlign = 'right'; }</script>"
+{
+	"title": "$TITLE",
+	"description": "$DESCRIPTION",
+	"author": "$AUTHOR",
+	"publisher": "$PUBLISHER",
+	
+	"pdf": {
+		"margin": {
+			"right": 62,
+			"left": 62,
+			"top": 62,
+			"bottom": 62
 		},
+		"headerTemplate": "<p id='ebook-header' style='border-bottom: 1px solid black; margin-top: 36pt;'><span class='odd_page'><span>Casa do Código</span><span style='float:right'>_SECTION_</span></span><span class='even_page'><span>_SECTION_</span><span style='float:right'>Casa do Código</span></span><script>if(!(/^[0-9]/.test('_SECTION_'))) { document.getElementById('ebook-header').style.display='none'; }</script></p>",
+		"footerTemplate": "<p id='ebook-footer'></p><script>var footer = document.getElementById('ebook-footer'); footer.innerHTML = _PAGENUM_ - 2; if(_PAGENUM_ % 2 != 0){ footer.style.textAlign = 'right'; }</script>"
+	},
 
-		"plugins": ["cdc", "$TEMA"],
-		
-		"links": {
-			"gitbook": false
-		}
+	"plugins": ["cdc", "$THEME"],
+	
+	"links": {
+		"gitbook": false
 	}
+}
 END
+
+# Empty cover
+if [ ! -f "$BUILDDIR"/cover.jpg ]; then
+	convert -size 32x32 xc:orange "$BUILDDIR"/cover.jpg
+fi
 
 # Build PDF
 docker run -v "$BUILDDIR":/data cdc/gitbook gitbook pdf
